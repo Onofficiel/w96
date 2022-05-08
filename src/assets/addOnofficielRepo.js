34da71fe02d73e705410699f115baef3551ea2bd
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2022-05-08 15:29:03 +0200

    ok

--- src/assets/addOnofficielRepo.js
+++ src/assets/addOnofficielRepo.js
@@ -2,7 +2,14 @@
   let srcJSON = JSON.parse(
     await w96.FS.readstr("c:/system/packages/configs/sources.json")
   );
-  srcJSON.sources.push("https://onofficiel.github.io/w96");
+
+  // Setting the correct repo version
+  if (w96.sys.env.getEnv("os_ver") >= 300000) {
+    srcJSON.sources.push("https://onofficiel.github.io/w96/v3");
+  } else {
+    srcJSON.sources.push("https://onofficiel.github.io/w96/v2");
+  }
+
   await w96.FS.writestr(
     "c:/system/packages/configs/sources.json",
     JSON.stringify(srcJSON)
