811bd0adea2bee6853da02e4c8d19d73dc5ae7c5
Kelbazz <its.kelbaz+code@gmail.com>
2024-01-21 16:13:12 +0100

    fix(website): missed the /w96

--- new/dist/src/js/index.js
+++ new/dist/src/js/index.js
@@ -2,4 +2,4 @@ const appTile = document.querySelector(".centered");
 const iframe = document.querySelector("iframe");
 
 // Init the iframe
-iframe.src = `https://windows96.net/?live=1&rootfs=${window.location.origin}/rootfs.zip`;
+iframe.src = `https://windows96.net/?live=1&rootfs=${window.location.origin}/w96/rootfs.zip`;
